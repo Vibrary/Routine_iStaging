4156f6e57c60837816c40f7ecc1fbf74089039ce
Vibrary <inori.ocean@gmail.com>
2022-03-25 17:21:00 +0800

    refactor `testImages`

--- cypress/integration/Upload panoramas.ts
+++ cypress/integration/Upload panoramas.ts
@@ -4,6 +4,33 @@ Cypress.on('uncaught:exception', (err, ruunable) => {
     return false;
 })
 
+const testImages = [
+    [
+        'test (1).jpg',
+        'test (2).jpg',
+        'test (3).jpg',
+        'test (4).jpg',
+        'test (5).jpg',
+        'test (6).jpg',
+        'test (7).jpg',
+        'test (8).jpg',
+        'test (9).jpg',
+        'test (10).jpg'
+    ],
+    [
+        'test (11).jpg',
+        'test (12).jpg',
+        'test (13).jpg',
+        'test (14).jpg',
+        'test (15).jpg',
+        'test (16).jpg',
+        'test (17).jpg',
+        'test (18).jpg',
+        'test (19).jpg',
+        'test (20).jpg'
+    ]
+]
+
 describe('test `Upload panoramas` function', () => {
     it('Upload panoramas', () => {
 
@@ -43,44 +70,17 @@ describe('test `Upload panoramas` function', () => {
 
                 case 0:
                     cy.get('#Panoramas')
-                        .attachFile(['test (1).jpg',
-                                    'test (2).jpg',
-                                    'test (3).jpg',
-                                    'test (4).jpg',
-                                    'test (5).jpg',
-                                    'test (6).jpg',
-                                    'test (7).jpg',
-                                    'test (8).jpg',
-                                    'test (9).jpg',
-                                    'test (10).jpg',]);
+                        .attachFile(testImages[0]);
                     break;
 
                 case 1:
                     cy.get('#Panoramas')
-                        .attachFile(['test (11).jpg',
-                                    'test (12).jpg',
-                                    'test (13).jpg',
-                                    'test (14).jpg',
-                                    'test (15).jpg',
-                                    'test (16).jpg',
-                                    'test (17).jpg',
-                                    'test (18).jpg',
-                                    'test (19).jpg',
-                                    'test (20).jpg',]);
+                        .attachFile(testImages[1]);
                     break;
 
                 default:
                     cy.get('#Panoramas')
-                        .attachFile(['test (1).jpg',
-                                    'test (2).jpg',
-                                    'test (3).jpg',
-                                    'test (4).jpg',
-                                    'test (5).jpg',
-                                    'test (6).jpg',
-                                    'test (7).jpg',
-                                    'test (8).jpg',
-                                    'test (9).jpg',
-                                    'test (10).jpg',]);
+                        .attachFile(testImages[0]);
 
             }
 
