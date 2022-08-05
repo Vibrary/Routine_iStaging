79837fb3dfe63b738714b077089fa037c486b165
Vibrary <inori.ocean@gmail.com>
2022-08-05 09:35:09 +0800

    daily test

--- cypress/pages/Date_pages.ts
+++ cypress/pages/Date_pages.ts
@@ -3,13 +3,15 @@ export class DatePage {
     selectDate(date : string) {
         if( date == 'today' )
             return today;
+        else if( date == 'month' )
+            return thisYear + '-' + thisMonth;
         else
             return 'test';
     }
 
 }
 
-const today = '2022-08-04';
+const today = '2022-08-05';
 const thisYear = '2022';
-const thisMonth = '4';
+const thisMonth = '08';
 const thisDay = '19';
